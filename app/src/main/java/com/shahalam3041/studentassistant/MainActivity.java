b277f83e57001fc894617bb2c915be813adca46a
ShahAlam <shahalam3041@gmail.com>
2018-03-18 00:19:20 +0600

    added a comment just to check github revert........

--- app/src/main/java/com/shahalam3041/studentassistant/MainActivity.java
+++ app/src/main/java/com/shahalam3041/studentassistant/MainActivity.java
@@ -23,6 +23,8 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /// adding comment just to check github commit that will be reverted........
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
